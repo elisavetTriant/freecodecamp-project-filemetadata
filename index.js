966382dbb2168b032851838c61b16a24fb4b2cba
elisavetTriant <elitriant@gmail.com>
2022-08-15 17:35:01 +0300

    Solution with multer

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+
+//Add multer 
+const multer = require('multer')
+//set up multer.
+const upload = multer({ dest: 'uploads_tmp/' }).single('upfile');
+//we will need this so we don't pollute the disk with dummy files
+const fs = require('fs');
 
 var app = express();
 
@@ -11,8 +18,36 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+//SOLUTION with multer
 
+app.post('/api/fileanalyse', async function (req, res) {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      res.json({ error: err });
+    } else if (err) {
+      // An unknown error occurred when uploading.
+      res.json({ error: "An unknown error occurred when uploading. " + err });
+    }
+    // Everything went fine.
+    fileData = req.file;
 
+    //Delete the uploaded file?
+    fs.unlink(fileData.path, (err => {
+      if (err)
+        console.log(err);
+      else {
+        console.log("File Deleted");
+      }
+    }))
+
+    //ok!
+    res.json({
+      name: fileData.originalname,
+      type: fileData.mimetype,
+      size: fileData.size
+    });
+  })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
